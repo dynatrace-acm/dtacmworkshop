ba3de1c38fcf9ec2e1d5d1c670201025879b436b
wmsegar <wayne.segar@gmail.com>
2019-08-28 16:35:03 -0400

    fixed path issue

--- utils/deploy-sockshop.sh
+++ utils/deploy-sockshop.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-kubectl create -f manifests/k8s-namespaces.yml
+kubectl create -f ../manifests/k8s-namespaces.yml
 
-kubectl apply -f manifests/backend-services/user-db/dev/
-#kubectl apply -f manifests/backend-services/user-db/production/
+kubectl apply -f ../manifests/backend-services/user-db/dev/
 
-kubectl apply -f manifests/backend-services/shipping-rabbitmq/dev/
-#kubectl apply -f manifests/backend-services/shipping-rabbitmq/production/
 
-kubectl apply -f manifests/backend-services/carts-db/
+kubectl apply -f ../manifests/backend-services/shipping-rabbitmq/dev/
 
-kubectl apply -f manifests/backend-services/catalogue-db/
 
-kubectl apply -f manifests/backend-services/orders-db/
+kubectl apply -f ../manifests/backend-services/carts-db/
 
-kubectl apply -f manifests/sockshop-app/dev/
-#kubectl apply -f manifests/sockshop-app/production/
+kubectl apply -f ../manifests/backend-services/catalogue-db/
+
+kubectl apply -f ../manifests/backend-services/orders-db/
+
+kubectl apply -f ../manifests/sockshop-app/dev/
+
 
