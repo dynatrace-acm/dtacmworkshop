6f6186c13f49bc08ca801461afa352888d6be7f7
wmsegar <wayne.segar@gmail.com>
2019-08-30 16:33:18 -0400

    removed Tower and Cleanup to new branch

--- 4-DeployTower/deployTower.sh
+++
@@ -1,3 +0,0 @@
-kubectl create -f ./manifests/ansible-tower/namespace.yml
-kubectl create -f ./manifests/ansible-tower/deployment.yml
-kubectl create -f ./manifests/anisble-tower/service.yml
--- 5-Cleanup/cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl delete ns dev
-kubectl delete ns production
-kubectl delete ns istio-system
