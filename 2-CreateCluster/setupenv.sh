d0c2d2f807b8c01ba93f969bf4dcfa49a92ffb6e
Wayne Segar <wayne.segar@gmail.com>
2019-09-06 21:07:15 +0000

    modifications to support AKS

--- 2-CreateCluster/setupenv.sh
+++ 2-CreateCluster/setupenv.sh
@@ -7,6 +7,27 @@ export TENANTID=$(cat ../1-Credentials/creds.json | jq -r '.dynatraceTenantID')
 export ENVIRONMENTID=$(cat ../1-Credentials/creds.json | jq -r '.dynatraceEnvironmentID')
 
 
+if hash gcloud 2>/dev/null; then
+    echo "Google Cloud"
+    export CLOUD_PROVIDER=GKE
+elif hash az 2>/dev/null; then
+    echo "Azure Cloud"
+    export CLOUD_PROVIDER=AKS
+else
+    echo "No supported Cloud Provider (GCP or AKS) detected."
+    exit 1;
+fi
+
+echo "Creating $CLOUD_PROVIDER Cluster with the following credentials: "
+echo "API_TOKEN = $API_TOKEN"
+echo "PAAS_TOKEN = $PAAS_TOKEN"
+echo "TENANTID = $TENANTID"
+echo "ENVIRONMENTID = $ENVIRONMENTID"
+echo "Cloud Provider $CLOUD_PROVIDER"
+
+echo ""
+read -p "Is this all correct? (y/n) : " -n 1 -r
+echo ""
 
 usage()
 {
@@ -14,27 +35,48 @@ usage()
     exit
 }
 
-echo "Creating GKE Cluster with the following credentials: "
-echo "API_TOKEN = $API_TOKEN"
-echo "PAAS_TOKEN = $PAAS_TOKEN"
-echo "TENANTID = $TENANTID"
-echo "ENVIRONMENTID = $ENVIRONMENTID"
-
-read -p "Is this all correct? (y/n) : " -n 1 -r
-
-echo ""
-
-if [[ $REPLY =~ ^[Yy]$ ]]; then
+deployGKE()
+{
     echo "Creating GKE Cluster..."
     gcloud container clusters create acmworkshop --zone=us-central1-a --num-nodes=1 --machine-type=n1-highmem-2 --image-type=Ubuntu
+
+    kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
+}
+
+deployAKS()
+{
+    echo "Creating AKS Cluster..."
+    export AKS_RESOURCE_GROUP=ACM
+    export AKS_CLUSTER_NAME=acmworkshop
+
+    az group create --name $AKS_RESOURCE_GROUP --location centralus
+    az aks create --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME --node-count 1 --node-vm-size Standard_B4ms --generate-ssh-keys
+
+    az aks get-credentials --resource-group $AKS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME
+}
+
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    case $CLOUD_PROVIDER in
+        GKE)
+        deployGKE
+        ;;
+        AKS)
+        deployAKS
+        ;;
+        *)
+        echo "No supported Cloud Provider (GCP or AKS) detected."
+        exit 1
+        ;;
+    esac
 else
     exit 1
 fi
 
-echo "Cluster created"
-echo "Deploying OneAgent Operator"
 
-kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
+echo "Cluster created"
+
+echo "Deploying OneAgent Operator"
 
 kubectl create namespace dynatrace
 
@@ -68,11 +110,11 @@ esac
 kubectl create -f cr.yaml
 
 echo "Waiting for OneAgent to startup..."
-
 sleep 120
 
 echo "Deploying SockShop Application"
-
 ../utils/deploy-sockshop.sh
 
 echo "Deployment Complete"
+
+
