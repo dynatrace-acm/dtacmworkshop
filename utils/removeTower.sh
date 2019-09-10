645542549296cf439cba9c5515e9761d6cbe1d03
wmsegar <wayne.segar@gmail.com>
2019-09-10 15:59:58 -0400

    updated removeTower script

--- utils/removeTower.sh
+++ utils/removeTower.sh
@@ -1,5 +1,10 @@
-!#/bin/bash
+#!/bin/bash
 
 kubectl delete -f ../manifests/ansible-tower/service.yml
 kubectl delete -f ../manifests/ansible-tower/deployment.yml
 kubectl delete -f ../manifests/ansible-tower/namespace.yml
+
+
+echo "----------------------------------------------------"
+echo "Ansible Tower has been removed"
+echo "----------------------------------------------------"
