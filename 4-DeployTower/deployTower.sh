d6d11f0829dba0eab9cd8f8f769846be38441800
wmsegar <wayne.segar@gmail.com>
2019-09-11 15:43:43 -0400

    updated tower scripts

--- 4-DeployTower/deployTower.sh
+++ 4-DeployTower/deployTower.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
+
 kubectl create -f ../manifests/ansible-tower/namespace.yml
 kubectl create -f ../manifests/ansible-tower/deployment.yml
 kubectl create -f ../manifests/ansible-tower/service.yml
 
-echo "Waiting for Ansible Tower to start..."
+echo "Waiting to Ansible Tower to start..."
+sleep 120
 
-sleep 60
-
-./configureAnsible.sh
+../utils/configureAnsible.sh
