3c945659228929ab090308317806a6f0f8a09d09
wmsegar <wayne.segar@gmail.com>
2019-10-02 17:22:01 -0400

    broke out Dynatrace Operator into separate scripts

---
+++ utils/deploy-dt-operator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+export API_TOKEN=$(cat ../1-Credentials/creds.json | jq -r '.dynatraceApiToken')
+export PAAS_TOKEN=$(cat ../1-Credentials/creds.json | jq -r '.dynatracePaaSToken')
+export TENANTID=$(cat ../1-Credentials/creds.json | jq -r '.dynatraceTenantID')
+export ENVIRONMENTID=$(cat ../1-Credentials/creds.json | jq -r '.dynatraceEnvironmentID')
+
+kubectl create namespace dynatrace
+
+#LATEST_RELEASE=$(curl -s https://api.github.com/repos/dynatrace/dynatrace-oneagent-operator/releases/latest | grep tag_name | cut -d '"' -f 4)
+LATEST_RELEASE=v0.3.1
+kubectl create -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/kubernetes.yaml
+
+kubectl -n dynatrace create secret generic oneagent --from-literal="apiToken="$API_TOKEN --from-literal="paasToken="$PAAS_TOKEN
+
+if [[ -f "cr.yaml" ]]; then
+    rm -f cr.yaml
+    echo "Removed cr.yaml"
+fi
+
+curl -o cr.yaml https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/cr.yaml
+
+case $ENVIRONMENTID in
+        '')
+        echo "SaaS Deplyoment"
+        sed -i 's/apiUrl: https:\/\/ENVIRONMENTID.live.dynatrace.com\/api/apiUrl: https:\/\/'$TENANTID'.live.dynatrace.com\/api/' cr.yaml
+        ;;
+        *)
+        echo "Managed Deployment"
+        sed -i 's/apiUrl: https:\/\/ENVIRONMENTID.live.dynatrace.com\/api/apiUrl: https:\/\/'$TENANTID'.dynatrace-managed.com\/e\/'$ENVIRONMENTID'\/api/' cr.yaml
+        ;;
+        ?)
+        usage
+        ;;
+esac
+
+kubectl create -f cr.yaml
