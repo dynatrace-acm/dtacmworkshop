d78848edfa1b546c435de952fbf34bef3d11e9ce
steve-caron-dynatrace <steve.caron@dynatrace.com>
2020-02-24 23:49:27 -0500

    Rollout deployment for carts production

--- 3-DeployJenkins/deployJenkins.sh
+++ 3-DeployJenkins/deployJenkins.sh
@@ -22,7 +22,7 @@ export JENKINS_POD=$(kubectl get po -n cicd --no-headers=true -o custom-columns=
 kubectl exec -it -n cicd $JENKINS_POD -- sed -i "s/JENKINS_SERVER_PLACEHOLDER/https:\/\/$K8S_MASTER/g" /var/jenkins_home/config.xml
 kubectl exec -it -n cicd $JENKINS_POD -- sed -i "s/JENKINS_URL_PLACEHOLDER/http:\/\/$POD_IP:8080/g" /var/jenkins_home/config.xml
 
-curl -s -XPOST http://$JENKINS_URL:$JENKINS_URL_PORT/createItem?name=DeploySockShop -u $JENKINS_USERNAME_DECODE:$JENKINS_PASSWORD_DECODE --data-binary @config.xml -H "Content-Type:text/xml"
+curl -s -XPOST http://$JENKINS_URL:$JENKINS_URL_PORT/createItem?name=DeployCarts -u $JENKINS_USERNAME_DECODE:$JENKINS_PASSWORD_DECODE --data-binary @config.xml -H "Content-Type:text/xml"
 
 #Restart Jenkins
 curl -X POST http://$JENKINS_URL:$JENKINS_URL_PORT/restart -u $JENKINS_USERNAME_DECODE:$JENKINS_PASSWORD_DECODE
