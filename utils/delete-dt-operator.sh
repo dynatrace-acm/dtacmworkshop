395b5f549051736185b8aae9cf4505d73ee742dd
steve-caron-dynatrace <steve.caron@dynatrace.com>
2020-02-25 23:13:54 -0500

    using latest version of operator

--- utils/delete-dt-operator.sh
+++ utils/delete-dt-operator.sh
@@ -2,7 +2,8 @@
 
 kubectl delete -n dynatrace oneagent --all
 
-LATEST_RELEASE=v0.3.1
+LATEST_RELEASE=$(curl -s https://api.github.com/repos/dynatrace/dynatrace-oneagent-operator/releases/latest | grep tag_name | cut -d '"' -f 4)
+
 kubectl delete -f https://raw.githubusercontent.com/Dynatrace/dynatrace-oneagent-operator/$LATEST_RELEASE/deploy/kubernetes.yaml
 
 kubectl delete namespace dynatrace
